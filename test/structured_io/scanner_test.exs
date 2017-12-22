e78984c2c356d2d21fcbd2ee0a4bd896ba62a668
Nils Jonsson <nils@alumni.rice.edu>
2017-12-22 14:54:38 -0600

    Rework load tests and increase their strain tenfold

--- test/structured_io/scanner_test.exs
+++ test/structured_io/scanner_test.exs
@@ -1,27 +1,4 @@
 defmodule StructuredIO.ScannerTest do
   use ExUnit.Case
   doctest StructuredIO.Scanner
-
-  alias StructuredIO.Scanner
-
-  @tag :slow
-  describe ".scan_across/3" do
-    test "with a large binary dataset" do
-      opener = <<0, 0, 0>>
-      closer = <<255, 255, 255>>
-      scan_data = opener                                          <>
-                  String.duplicate(<<0, 0, 0, 1, 2, 3>>, 524_288) <>
-                  closer
-      assert Scanner.scan_across(scan_data, opener, closer) == {scan_data, ""}
-    end
-  end
-
-  @tag :slow
-  describe ".scan_through/3" do
-    test "with a large binary dataset" do
-      delimiter = <<255, 255, 255>>
-      scan_data = String.duplicate(<<0, 0, 0, 1, 2, 3>>, 524_288) <> delimiter
-      assert Scanner.scan_through(scan_data, delimiter) == {scan_data, ""}
-    end
-  end
 end
--- test/structured_io_test.exs
+++ test/structured_io_test.exs
@@ -7,6 +7,14 @@ defmodule StructuredIOTest do
     {:ok, structured_io: structured_io}
   end
 
+  def with_random_io_lists(%{size: size, count: count}, fun) do
+    for _ <- 1..count do
+      0..255
+      |> Enum.take_random(size)
+      |> fun.()
+    end
+  end
+
   describe ".binread_across/3" do
     test "before .binwrite/2", %{structured_io: structured_io} do
       opener = <<0, 0, 0>>
@@ -18,13 +26,25 @@ defmodule StructuredIOTest do
     test "with a large dataset", %{structured_io: structured_io} do
       opener = <<0, 0, 0>>
       closer = <<255, 255, 255>>
-      scan_data = opener                                          <>
-                  String.duplicate(<<0, 0, 0, 1, 2, 3>>, 524_288) <>
-                  closer
-      :ok = StructuredIO.binwrite(structured_io, scan_data)
-      assert StructuredIO.binread_across(structured_io,
-                                         opener,
-                                         closer) == scan_data
+      :ok = StructuredIO.binwrite(structured_io, opener)
+      with_random_io_lists %{size: 100, count: 200_000}, fn io_list ->
+        :ok = StructuredIO.binwrite(structured_io, io_list)
+      end
+      :ok = StructuredIO.binwrite(structured_io, closer)
+      result = StructuredIO.binread_across(structured_io,
+                                           opener,
+                                           closer,
+                                           12_000)
+      result_byte_size = byte_size(result)
+      assert result_byte_size == byte_size(opener) +
+                                 20_000_000        +
+                                 byte_size(closer)
+      beginning_of_result = binary_part(result, 0, byte_size(opener))
+      assert beginning_of_result == opener
+      end_of_result = binary_part(result,
+                                  byte_size(opener) + 20_000_000,
+                                  byte_size(closer))
+      assert end_of_result == closer
     end
 
     test "after .write/2", %{structured_io: structured_io} do
@@ -46,10 +66,18 @@ defmodule StructuredIOTest do
 
     @tag :slow
     test "with a large dataset", %{structured_io: structured_io} do
-      delimiter = <<255, 255, 255>>
-      scan_data = String.duplicate(<<0, 0, 0, 1, 2, 3>>, 524_288) <> delimiter
-      :ok = StructuredIO.binwrite(structured_io, scan_data)
-      assert StructuredIO.binread_through(structured_io, delimiter) == scan_data
+      delimiter = <<255, 255, 255, 255, 255>>
+      with_random_io_lists %{size: 100, count: 200_000}, fn io_list ->
+        :ok = StructuredIO.binwrite(structured_io, io_list)
+      end
+      :ok = StructuredIO.binwrite(structured_io, delimiter)
+      result = StructuredIO.binread_through(structured_io, delimiter, 12_000)
+      result_byte_size = byte_size(result)
+      assert result_byte_size == 20_000_000 + byte_size(delimiter)
+      end_of_result = binary_part(result,
+                                  result_byte_size - byte_size(delimiter),
+                                  byte_size(delimiter))
+      assert end_of_result == delimiter
     end
 
     test "after .write/2", %{structured_io: structured_io} do
