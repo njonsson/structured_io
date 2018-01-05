1d15b87aa4d5209ec73fda6b00d89f10733df3b0
Nils Jonsson <nils@alumni.rice.edu>
2018-01-04 19:40:32 -0600

    Eliminate needless performance tests of deprecated APIs

--- test/structured_io/deprecated_test.exs
+++ test/structured_io/deprecated_test.exs
@@ -23,31 +23,6 @@ defmodule StructuredIO.DeprecatedTest do
       assert StructuredIO.binread_across(structured_io, opener, closer) == ""
     end
 
-    @tag :slow
-    test "with a large dataset", %{structured_io: structured_io} do
-      opener = <<0, 0, 0>>
-      closer = <<255, 255, 255>>
-      :ok = StructuredIO.binwrite(structured_io, opener)
-      with_random_io_lists %{size: 100, count: 200_000}, fn io_list ->
-        :ok = StructuredIO.binwrite(structured_io, io_list)
-      end
-      :ok = StructuredIO.binwrite(structured_io, closer)
-      result = StructuredIO.binread_across(structured_io,
-                                           opener,
-                                           closer,
-                                           12_000)
-      result_byte_size = byte_size(result)
-      assert result_byte_size == byte_size(opener) +
-                                 20_000_000        +
-                                 byte_size(closer)
-      beginning_of_result = binary_part(result, 0, byte_size(opener))
-      assert beginning_of_result == opener
-      end_of_result = binary_part(result,
-                                  result_byte_size - byte_size(closer),
-                                  byte_size(closer))
-      assert end_of_result == closer
-    end
-
     test "in Unicode mode", %{structured_io: structured_io} do
       opener = "<elem>"
       closer = "</elem>"
@@ -64,29 +39,6 @@ defmodule StructuredIO.DeprecatedTest do
       opener = <<0, 0, 0>>
       closer = <<255, 255, 255>>
       assert StructuredIO.binread_between(structured_io, opener, closer) == ""
-    end
-
-    @tag :slow
-    test "with a large dataset", %{structured_io: structured_io} do
-      opener = <<0, 0, 0>>
-      closer = <<255, 255, 255>>
-      :ok = StructuredIO.binwrite(structured_io, opener)
-      with_random_io_lists %{size: 100, count: 200_000}, fn io_list ->
-        :ok = StructuredIO.binwrite(structured_io, io_list)
-      end
-      :ok = StructuredIO.binwrite(structured_io, closer)
-      result = StructuredIO.binread_between(structured_io,
-                                            opener,
-                                            closer,
-                                            12_000)
-      result_byte_size = byte_size(result)
-      assert result_byte_size == 20_000_000
-      beginning_of_result = binary_part(result, 0, byte_size(opener))
-      refute beginning_of_result == opener
-      end_of_result = binary_part(result,
-                                  result_byte_size - byte_size(closer),
-                                  byte_size(closer))
-      refute end_of_result == closer
     end
 
     test "in Unicode mode", %{structured_io: structured_io} do
@@ -106,22 +58,6 @@ defmodule StructuredIO.DeprecatedTest do
       assert StructuredIO.binread_through(structured_io, delimiter) == ""
     end
 
-    @tag :slow
-    test "with a large dataset", %{structured_io: structured_io} do
-      delimiter = <<255, 255, 255, 255, 255>>
-      with_random_io_lists %{size: 100, count: 200_000}, fn io_list ->
-        :ok = StructuredIO.binwrite(structured_io, io_list)
-      end
-      :ok = StructuredIO.binwrite(structured_io, delimiter)
-      result = StructuredIO.binread_through(structured_io, delimiter, 12_000)
-      result_byte_size = byte_size(result)
-      assert result_byte_size == 20_000_000 + byte_size(delimiter)
-      end_of_result = binary_part(result,
-                                  result_byte_size - byte_size(delimiter),
-                                  byte_size(delimiter))
-      assert end_of_result == delimiter
-    end
-
     test "in Unicode mode", %{structured_io: structured_io} do
       delimiter = "<br/>"
       scan_data = "foo" <> delimiter
@@ -136,22 +72,6 @@ defmodule StructuredIO.DeprecatedTest do
     test "before .binwrite/2", %{structured_io: structured_io} do
       opener = <<0, 0, 0>>
       assert StructuredIO.binread_to(structured_io, opener) == ""
-    end
-
-    @tag :slow
-    test "with a large dataset", %{structured_io: structured_io} do
-      opener = <<0, 0, 0, 0, 0>>
-      with_random_io_lists %{size: 100, count: 200_000}, fn io_list ->
-        :ok = StructuredIO.binwrite(structured_io, io_list)
-      end
-      :ok = StructuredIO.binwrite(structured_io, opener)
-      result = StructuredIO.binread_to(structured_io, opener, 12_000)
-      result_byte_size = byte_size(result)
-      assert result_byte_size == 20_000_000
-      end_of_result = binary_part(result,
-                                  result_byte_size - byte_size(opener),
-                                  byte_size(opener))
-      assert end_of_result != opener
     end
 
     test "in Unicode mode", %{structured_io: structured_io} do
